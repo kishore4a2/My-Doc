f6da7513ae71b3616adec9783567b8b87652c16e
Kishore <kishore@prestomall.com>
2019-12-07 19:07:22 +0800

    > Install Sync pod for coredata

---
+++ MyDocNYT/Pods/Target Support Files/Pods-MyDocNYT/Pods-MyDocNYT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyDocNYT : NSObject
+@end
+@implementation PodsDummy_Pods_MyDocNYT
+@end
---
+++ MyDocNYT/Pods/Target Support Files/Pods-MyDocNYTTests/Pods-MyDocNYTTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyDocNYTTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyDocNYTTests
+@end
